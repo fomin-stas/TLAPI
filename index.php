7227f071a303f54a3ec0321c0a4fbae720e96bc0
fomin-stas <fomin.stanislav@gmail.com>
2015-07-17 11:16:49 +0300

    Mini fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <body>
         <?php
         require_once('./TravelLineAPI.php');
-        $travel = new TravelLineAPI("TlConnect.2690","KjlQbU53Kigv",2690);
+        $travel = new TravelLineAPI();
 
 /*#######################################################################################
 =========================================================================================
@@ -45,11 +45,11 @@
 //        ));
 
 /*Результат приходит, но о фильтрации по типу ничего сказать не могу*/
-//        $result = $travel->ReadReservation(array(
-//            'Start' => '2012-01-02',
-//            'End' => '2016-01-02',
-//            'DateType' => 'LastUpdateDate'
-//        ));
+        $result = $travel->ReadReservation(array(
+            'Start' => '2012-01-02',
+            'End' => '2016-01-02',
+            'DateType' => 'LastUpdateDate'
+        ));
 
 
 /*-------------------CancelReservation()------------------*/
@@ -57,54 +57,52 @@
 Если посылать Amount или EchoTocken, то лезут ошибки из nusoap. Если посылать только ID, то какой-то ответ приходит.
 Что-то не работает.*/
 //        $result = $travel->CancelReservation(array(
-////            'EchoTocken' => 'echo', //this part cause error. Without it something works
-//            'ID' => '1234567777', //returns 'Booking #1234567777 not found'
-////            'ID' => '20150709-2690-671648', //returns 'Failed to cancel booking #20150709-2690-671648'
-////            'Amount' => 25, //and this
-////            'CurrencyCode' => 'RUB' //but this does not!
+//            
+//            'ID' => '20150721-2690-672467'
+//           
 //        ));
 
 
 /*----------------------NotifReport()---------------------*/
 
-        $result = $travel->NotifReport(array(
-//            'HotelCode'=>'TRAVELLINE',
-            'EchoTocken' => 'echo',
-            'Status' => 'Success', //Success or Error
-            'Warnings' => array('warning1', 'warning2', 'warning3'),
-            'HotelReservations' => array(
-/*Я хотел отдавать массив из таких массивов, но это не работает. Функция, которая строит из всего этого XML такое не съедает. Так что сейчас есть возможность отдавать только один элемент HotelReservation.*/
-                array(
-                    'ID' => '20150712-2690-671651q',
-                    'CreateDateTime'=>'2015-07-08T14:55:49.873',
-                    /*ResStatus знает только Reserved. Остальные значения выдают ошибку*/
-                    'ResStatus' => 'Reserved', //possible: Reserved, Cancelled, Checkedout, Inhouse, Requestdenied, Waitlisted,
-                    'LastModifyDateTime' => '2015-07-08T14:55:50.187',
-                    'RoomStays' => array(
-                        array('IndexNumber' => '705449')
-                    ),
-                    'ResGlobalInfo' => array(
-                        'ResID_Value' => 'ID1', //Здесь должен быть идентификатор брони в АСУ.
-//                        'Comments' => 'Test' //Почему-то препятствует работе.
-                    )
-                ),
+//        $result = $travel->NotifReport(array(
+////          'HotelCode'=>'TRAVELLINE',
+//            'EchoTocken' => 'echo',
+//            'Status' => 'Success', //Success or Error
+//            'Warnings' => array('warning1', 'warning2', 'warning3'),
+//            'HotelReservations' => array(
+///*Я хотел отдавать массив из таких массивов, но это не работает. Функция, которая строит из всего этого XML такое не съедает. Так что сейчас есть возможность отдавать только один элемент HotelReservation.*/
 //                array(
-//                    'ID' => '20150709-2690-671648',
-//                    'CreateDateTime'=>'2015-07-08T14:53:55.723',
-//                    'ResStatus' => 'Cancelled',
-//                    'LastModifyDateTime' => '2015-07-08T14:53:56.097',
+//                    'ID' => '20150712-2690-671651q',
+//                    'CreateDateTime'=>'2015-07-08T14:55:49.873',
+//                    /*ResStatus знает только Reserved. Остальные значения выдают ошибку*/
+//                    'ResStatus' => 'Reserved', //possible: Reserved, Cancelled, Checkedout, Inhouse, Requestdenied, Waitlisted,
+//                    'LastModifyDateTime' => '2015-07-08T14:55:50.187',
 //                    'RoomStays' => array(
-//                        'IndexNumber' => '705446'
+//                        array('IndexNumber' => '705449')
+//                    ),
+//                    'ResGlobalInfo' => array(
+//                        'ResID_Value' => 'ID1', //Здесь должен быть идентификатор брони в АСУ.
+////                        'Comments' => 'Test' //Почему-то препятствует работе.
 //                    )
 //                ),
-//                array(
-//                    'ID' => '123',
-//                    'CreateDateTime'=>'2014-09-30T00:45:30+02:00',
-//                    'ResStatus' => 'Checkedout',
-//                    'LastModifyDateTime' => '2014-09-30T01:43:43.323'
-//                )
-            )
-        ));
+////                array(
+////                    'ID' => '20150709-2690-671648',
+////                    'CreateDateTime'=>'2015-07-08T14:53:55.723',
+////                    'ResStatus' => 'Cancelled',
+////                    'LastModifyDateTime' => '2015-07-08T14:53:56.097',
+////                    'RoomStays' => array(
+////                        'IndexNumber' => '705446'
+////                    )
+////                ),
+////                array(
+////                    'ID' => '123',
+////                    'CreateDateTime'=>'2014-09-30T00:45:30+02:00',
+////                    'ResStatus' => 'Checkedout',
+////                    'LastModifyDateTime' => '2014-09-30T01:43:43.323'
+////                )
+//            )
+//        ));
 
 
 /*--------------------------------------------------------*/
